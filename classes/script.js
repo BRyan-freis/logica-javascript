594dfd1ffe02bbde424a8f9869a601c920a3ff98
BRYAN FERREIRA DOS REIS <bryan.freis@senacsp.edu.br>
2025-06-11 17:18:58 -0300

    update

---
+++ classes/script.js
@@ -0,0 +1,37 @@
+// Declarando a classe
+class Pessoa {
+    constructor(nome,idade){
+        this.nome = nome
+        this.idade = idade
+    }
+
+    // Declarando um metódo
+    saudar(){
+        return `Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`;
+    }
+}
+
+// Instânciando
+const pessoa1 = new Pessoa ("Bryan", 78);
+const pessoa2 = new Pessoa ("Luan", 25);
+console.log(pessoa1.saudar());
+console.log(pessoa2.saudar());
+
+// Nova classe
+class Carro {
+
+    // Atributos
+    constructor(marca, modelo) {
+        this.marca = marca
+        this.modelo = modelo
+    }
+
+    // Método
+    exibirInfo() {
+        return `Carro: ${this.marca} ${this.modelo}`
+    }
+}
+
+// Instância
+const meuCarro = new Carro("Fusca", "1974")
+console.log(meuCarro.exibirInfo())
